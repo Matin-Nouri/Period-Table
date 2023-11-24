d00caed97fef4fcd4ec2391d060475db8b645429
Matin Nouri <matin.no.al@gmail.com>
2023-11-24 16:28:46 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt update
+sudo apt install python3 pip
+
+sudo dnf install python3 pip
+
+sudo pip install requests pdfkit pandas
+sudo apt install wkhtmltopdf
+sudo dnf install wkhtmltopdf
